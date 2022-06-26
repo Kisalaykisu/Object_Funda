a92b688e7beec16d9423f1e4ff80ca87d96d90cc
Kisalay <kisalaykisu@gmail.com>
2022-06-26 20:43:17 +0530

    Deck Of cards program has been added

--- src/com/bridgelabz/Object_Funda.java
+++ src/com/bridgelabz/Object_Funda.java
@@ -1,29 +1,59 @@
 package com.bridgelabz; // package
 
-import java.util.Scanner;  //Import Scanner
+import java.util.Arrays;  //import array
+import java.util.Random;  // import random
+
 public class Object_Funda {
-    static double accountBalance;
+    String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    static String[] array1D = {"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
+    static String[][] array2D = new String[4][9];
+
     public static void main(String[] args){ //Entry Point Of program
-        Object_Funda obj = new Object_Funda();  //Obj creation
-        Scanner sc = new Scanner(System.in);    // Make Scanner obj
-        System.out.println("Enter the account balance: ");
-        accountBalance = sc.nextDouble();  //Input Double
-        obj.debit();
-        System.out.println("Account Balance = " + accountBalance);
+        Object_Funda  obj = new Object_Funda(); //obj creation
+        obj.generateRandomCards();
     }
 
     /**
-     * debit method to debit amount from the account balance
+     * random card method is used to generate a random card from a deck of 52 cards
+     * @return random card method returns a card of String type
      */
-    public void debit() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the debit amount: ");
-        int debitAmount = sc.nextInt();
-        if (debitAmount < accountBalance) {
-            accountBalance -= debitAmount;
-        } else {
-            System.out.println("Debit amount exceeded account balance.");
-        }
+    public String randomCard() {
+        Random rand = new Random();
+        return suit[rand.nextInt(4)] + " " + rank[rand.nextInt(13)];
+    }
 
+    /**
+     * Method to display the cards received by the 4 players
+     */
+    public void showPlayerCards() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 9; j++) {
+                array2D[i][j] = array1D[(i + 1) * (j + 1) - 1];
+            }
+        }
+        int i = 1;
+        for (String[] row : array2D) {
+            System.out.println("Player " + i + ": " + Arrays.toString(row) + " \n");
+            i++;
+        }
+    }
+
+    /**
+     * Method to generate the required no.of cards and distribute them to the 4 players
+     */
+    public void generateRandomCards() {
+        Object_Funda  obj = new Object_Funda();
+        for (int i = 0; i < 36; i++) {
+            String x = obj.randomCard();
+            for (int j = 0; j <= i; j++) {
+                if (array1D[j].equals(x)) {
+                    x = obj.randomCard();
+                    j = 0;
+                }
+            }
+            array1D[i] = x;
+        }
+        obj.showPlayerCards();
     }
 }
