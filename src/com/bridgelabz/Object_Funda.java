bb9346a71b16005315d7160f5f8b49be479a712d
Kisalay <kisalaykisu@gmail.com>
2022-06-26 20:33:51 +0530

    Account Balance Case :

--- src/com/bridgelabz/Object_Funda.java
+++ src/com/bridgelabz/Object_Funda.java
@@ -1,7 +1,29 @@
-package com.bridgelabz;
+package com.bridgelabz; // package
 
+import java.util.Scanner;  //Import Scanner
 public class Object_Funda {
-    public static void main(String[] args){
-        System.out.println("Welcome to the Object Oriented program");
+    static double accountBalance;
+    public static void main(String[] args){ //Entry Point Of program
+        Object_Funda obj = new Object_Funda();  //Obj creation
+        Scanner sc = new Scanner(System.in);    // Make Scanner obj
+        System.out.println("Enter the account balance: ");
+        accountBalance = sc.nextDouble();  //Input Double
+        obj.debit();
+        System.out.println("Account Balance = " + accountBalance);
+    }
+
+    /**
+     * debit method to debit amount from the account balance
+     */
+    public void debit() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the debit amount: ");
+        int debitAmount = sc.nextInt();
+        if (debitAmount < accountBalance) {
+            accountBalance -= debitAmount;
+        } else {
+            System.out.println("Debit amount exceeded account balance.");
+        }
+
     }
 }
